f4ac01c12ab5a4abe990c4cb82354ac8f67b57db
NewPizza <gx109@naver.com>
2021-08-11 20:43:57 +0900

    MINO 그리기 완료, 회전 구현중

--- 0_TETRIS/0_TETRIS_0.c
+++ 0_TETRIS/0_TETRIS_0.c
@@ -2,6 +2,15 @@
 #include <windows.h>
 void prt_field();
 void gotoxy(int x_data, int y_data);
+void mino_I(int px, int py, int rot);
+void mino_O(int px, int py);
+void mino_Z(int px, int py);
+void mino_S(int px, int py);
+void mino_L(int px, int py);
+void mino_J(int px, int py);
+void mino_T(int px, int py);
+
+
 #define field_x 10
 #define field_y 20
 boolean field_data[field_x][field_y];
@@ -11,11 +20,12 @@ int y = 0;
 void main(void)
 {
     system("chcp 65001"); //윈도우 cmd 창 UTF-8로 설정
+    system("mode con cols=180 lines=60");
     system("cls");
+    mino_T(5, 5);
 
     prt_field();
-    gotoxy(12,12);
-    printf("a");
+
 }
 
 void prt_field()
@@ -41,4 +51,70 @@ void gotoxy(int x_data, int y_data)
     COORD pos={x_data,y_data}; 
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 
+}
+
+void mino_I(int px, int py, int rot)
+{
+    if(rot == 2){rot = 0;}
+    switch (rot)
+    {
+        case 2 : rot = 0;
+    case 0 :
+    field_data[px][py-3] = 1;
+    field_data[px][py-2] = 1;
+    field_data[px][py-1] = 1;
+    field_data[px][py] = 1;
+            break;
+    case 1 :
+    field_data[px][py-3] = 1;
+    field_data[px][py-2] = 1;
+    field_data[px][py-1] = 1;
+    field_data[px][py] = 1;
+    default:
+        break;
+    }
+
+    
+}
+void mino_O(int px, int py)
+{
+    field_data[px][py] = 1;
+    field_data[px-1][py] = 1;
+    field_data[px-1][py-1] = 1;
+    field_data[px][py-1] = 1;
+}
+void mino_Z(int px, int py)
+{
+    field_data[px][py] = 1;
+    field_data[px-1][py] = 1;
+    field_data[px-1][py-1] = 1;
+    field_data[px-2][py-1] = 1;
+}
+void mino_S(int px, int py)
+{
+    field_data[px][py] = 1;
+    field_data[px-1][py] = 1;
+    field_data[px][py-1] = 1;
+    field_data[px+1][py-1] = 1;
+}
+void mino_L(int px, int py)
+{
+    field_data[px][py] = 1;
+    field_data[px-1][py] = 1;
+    field_data[px-1][py-1] = 1;
+    field_data[px-1][py-2] = 1;
+}
+void mino_J(int px, int py)
+{
+    field_data[px][py] = 1;
+    field_data[px-1][py] = 1;
+    field_data[px][py-1] = 1;
+    field_data[px][py-2] = 1;
+}
+void mino_T(int px, int py)
+{
+    field_data[px][py] = 1;
+    field_data[px-1][py] = 1;
+    field_data[px+1][py] = 1;
+    field_data[px][py-1] = 1;
 }
